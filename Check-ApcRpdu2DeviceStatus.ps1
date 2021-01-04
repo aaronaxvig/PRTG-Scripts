74467aaa630f4ae2eb40ef2397e70a7a6d96168a
Aaron Axvig <aarona@cloverdalefoods.com>
2021-01-04 11:09:12 -0600

    Add Check-ApcRpdu2DeviceStatus and supporting files

---
+++ Check-ApcRpdu2DeviceStatus.ps1
@@ -0,0 +1,122 @@
+#Requires -Modules SNMPv3
+# Install-Module SNMPv3
+# https://www.powershellgallery.com/packages/SNMPv3
+
+#Requires -Modules PrtgXml
+# Install-Module PrtgXml
+# https://www.powershellgallery.com/packages/PrtgXml
+# https://github.com/lordmilko/PrtgXml
+
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingPlainTextForPassword", "SnmpCredentialsFile")]
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]
+    $Target,
+
+    [Parameter(Mandatory=$false)]
+    [string]
+    $SnmpCredentialsFile = 'SnmpCredentials.ps1'
+)
+
+$path = Join-Path $PSScriptRoot $SnmpCredentialsFile
+. $path
+
+$SnmpInfo.Target = $Target
+
+$SnmpInfo.OID = '1.3.6.1.4.1.318.1.1.26.3.0'
+$results = Invoke-SNMpv3Get @SnmpInfo
+$deviceCount = [int]$results.Value.ToString()
+
+$SnmpInfo.OID = '1.3.6.1.4.1.318.1.1.26.4.3.1'
+$results = Invoke-SNMPv3Walk @SnmpInfo
+
+Prtg {
+    for ($device = 1; $device -le $deviceCount; $device++) {
+        Result {
+            Channel "Device $device load state"
+            $value = $results | Where-Object { $_.OID -eq "1.3.6.1.4.1.318.1.1.26.4.3.1.4.$device" } | Select-Object -ExpandProperty Value
+            Value ([int]$value.ToString())
+            Unit 'Custom'
+            ValueLookup "oid.powernet-mib.rpdu2devicestatus.rpdu2devicestatusloadstate"
+        }
+        Result {
+            Channel "Device $device power"
+            $value = $results | Where-Object { $_.OID -eq "1.3.6.1.4.1.318.1.1.26.4.3.1.5.$device" } | Select-Object -ExpandProperty Value
+            Value ([decimal]$value.ToString() / 100)
+            Unit 'Custom'
+            CustomUnit 'Kilowatts'
+            Float 1
+        }
+        Result {
+            Channel "Device $device peak power"
+            $value = $results | Where-Object { $_.OID -eq "1.3.6.1.4.1.318.1.1.26.4.3.1.6.$device" } | Select-Object -ExpandProperty Value
+            Value ([decimal]$value.ToString() / 100)
+            Unit 'Custom'
+            CustomUnit 'Kilowatts'
+            Float 1
+        }
+        Result {
+            Channel "Device $device time since peak power"
+            $value = $results | Where-Object { $_.OID -eq "1.3.6.1.4.1.318.1.1.26.4.3.1.7.$device" } | Select-Object -ExpandProperty Value
+            Value ([int]((Get-Date) - [datetime]::ParseExact($value, 'MM/dd/yyyy HH:mm:ss', $null)).TotalSeconds)
+            Unit 'TimeSeconds'
+        }
+        Result {
+            Channel "Device $device time since peak power reset"
+            $value = $results | Where-Object { $_.OID -eq "1.3.6.1.4.1.318.1.1.26.4.3.1.8.$device" } | Select-Object -ExpandProperty Value
+            Value ([int]((Get-Date) - [datetime]::ParseExact($value, 'MM/dd/yyyy HH:mm:ss', $null)).TotalSeconds)
+            Unit 'TimeSeconds'
+        }
+        Result {
+            Channel "Device $device energy used"
+            $value = $results | Where-Object { $_.OID -eq "1.3.6.1.4.1.318.1.1.26.4.3.1.9.$device" } | Select-Object -ExpandProperty Value
+            Value ([decimal]$value.ToString() / 10)
+            Unit 'Custom'
+            CustomUnit 'Kilowatt-hours'
+            Float 1
+        }
+        Result {
+            Channel "Device $device time since energy used reset"
+            $value = $results | Where-Object { $_.OID -eq "1.3.6.1.4.1.318.1.1.26.4.3.1.10.$device" } | Select-Object -ExpandProperty Value
+            Value ([int]((Get-Date) - [datetime]::ParseExact($value, 'MM/dd/yyyy HH:mm:ss', $null)).TotalSeconds)
+            Unit 'TimeSeconds'
+        }
+        Result {
+            Channel "Device $device power supply alarm"
+            $value = $results | Where-Object { $_.OID -eq "1.3.6.1.4.1.318.1.1.26.4.3.1.12.$device" } | Select-Object -ExpandProperty Value
+            Value ([int]$value.ToString())
+            Unit 'Custom'
+            ValueLookup "oid.powernet-mib.rpdu2devicestatus.rpdu2devicestatuspowersupplyalarm"
+        }
+        Result {
+            Channel "Device $device power supply 1 status"
+            $value = $results | Where-Object { $_.OID -eq "1.3.6.1.4.1.318.1.1.26.4.3.1.13.$device" } | Select-Object -ExpandProperty Value
+            Value ([int]$value.ToString())
+            Unit 'Custom'
+            ValueLookup "oid.powernet-mib.rpdu2devicestatus.rpdu2devicestatuspowersupply1status"
+        }
+        Result {
+            Channel "Device $device power supply 2 status"
+            $value = $results | Where-Object { $_.OID -eq "1.3.6.1.4.1.318.1.1.26.4.3.1.14.$device" } | Select-Object -ExpandProperty Value
+            Value ([int]$value.ToString())
+            Unit 'Custom'
+            ValueLookup "oid.powernet-mib.rpdu2devicestatus.rpdu2devicestatuspowersupply1status"
+        }
+        Result {
+            Channel "Device $device apparent power"
+            $value = $results | Where-Object { $_.OID -eq "1.3.6.1.4.1.318.1.1.26.4.3.1.16.$device" } | Select-Object -ExpandProperty Value
+            Value ([decimal]$value.ToString() / 100)
+            Unit 'Custom'
+            CustomUnit 'KiloVolt-Amps'
+            Float 1
+        }
+        Result {
+            Channel "Device $device power factor"
+            $value = $results | Where-Object { $_.OID -eq "1.3.6.1.4.1.318.1.1.26.4.3.1.17.$device" } | Select-Object -ExpandProperty Value
+            Value ([decimal]$value.ToString() / 100)
+            Unit 'Custom'
+            CustomUnit ''
+            Float 1
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -1,6 +1,7 @@
 # To use
 
 * Copy all the PowerShell files into `%ProgramFiles x86%\PRTG Network Monitor\Custom Sensors\EXEXML` on your PRTG server.
+* Copy all the lookup files in `lookups` into `%ProgramFiles x86%\PRTG Network Monitor\lookups\custom` and load them (PRTG > Setup > Administrative Tools > Load Lookups and File Lists > Go!).
 * Rename `SnmpCredentialsExample.ps1` to `SnmpCredentials.ps1` and then edit its contents to provide SNMPv3 credential info for logging in to your devices.
 * Open Windows Powershell (x86) on your server as administrator and run two commands to install required Powershell modules:
     * Install-Module PrtgXml
@@ -13,7 +14,7 @@
 
 # Troubleshooting
 
-To log issues with the script, make sure the Result Handling setting on the sensor is set to `Store result` (stores in cases of goor results or errors) or `Store result in case of error` (stores only for an error).  Those logs can be found in `C:\ProgramData\Paessler\PRTG Network Monitor\Logs\sensors`.
+To log issues with the script, make sure the Result Handling setting on the sensor is set to `Store result` (stores in cases of goor results or errors) or `Store result in case of error` (stores only for an error).  Those logs can be found in `%ProgramData%\Paessler\PRTG Network Monitor\Logs\sensors`.
 
 Another useful technique is to manually run the script.  In Windows PowerShell (x86):
 ````
